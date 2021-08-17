e0d3df0bae38acd0cd459d23b7547f65ecdade75
Pasangsherpa123 <Pasangsherpa12580@gmail.com>
2021-08-17 19:20:25 +0545

    registration

--- code/sign_up.h
+++ code/sign_up.h
@@ -4,75 +4,81 @@
 #include <stdlib.h>
 struct Register
 {
-    char name[100];
-    char email[100];
-    char password[100];
+    char firstname[30];
+    char lastname[30];
+    char email[50];
+    char username[30];
+    char password[30];
+    int total_answers;
+    int correct_answers;
+    int incorrect_answers;
+    int score;
+    int admin;
+};
 
-} reg;
-
-int main()
+int registration(struct Register reg)
 {
-    char email[100], password1[100], password2[100], validate[100];
-    char gmail[] = "@gmail.com";
-    char email1[] = "@email.com";
+    // char email[100], password1[100], password2[100], validate[100];
+    // char gmail[] = "@gmail.com";
+    // char email1[] = "@email.com";
     FILE *user;
-      
+
     // open file for writing
-    user = fopen ("user.dat", "w");
+    user = fopen("resources/user.txt", "ab");
     if (user == NULL)
     {
         fprintf(stderr, "\nError opend file\n");
-        exit (1);
+        exit(1);
     }
 
-    printf("Register for more challenges: \n");
-    printf("Enter name: \n");
-    scanf("%s", &reg.name);
-    printf("Enter email \n");
-    scanf("%s", &email);
-    int length = strlen(email);
-    int i = length-10, j = 0;
-    while (i < length)
+    // printf("Register for more challenges: \n");
+    // printf("Enter name: \n");
+    // scanf("%s", &reg.name);
+    // printf("Enter email \n");
+    // scanf("%s", &email);
+    // int length = strlen(email);
+    // int i = length-10, j = 0;
+    // while (i < length)
+    // {
+    //     validate[j] = email[i];
+    //     j++;
+    //     i++;
+    // }
+    // int result = strcmp(validate, email1);
+    // int result1 = strcmp(validate, gmail);
+
+    // if (result == 0 || result1 == 0){
+    //     strcpy(reg.email, email);
+
+    //     printf("Enter password \n");
+    //     scanf("%s", &password1);
+    //     printf("Enter password once again to confirm \n");
+    //     scanf("%s", &password2);
+    //     if (strcmp(password1, password2) == 0)
+    //     {
+    //         strcpy(reg.password, password1);
+    //         printf("User registered successfully \n");
+    //     }
+    //     else
+    //     {
+    //         printf("Password and confirm password doesnot match!\n");
+    //     }
+    // }
+    // else
+    // {
+    //     printf("Email should contain @gmail.com or @email.com \n");
+    // }
+    // write struct to file
+    fwrite(&reg, sizeof(reg), 1, user);
+
+    if (fwrite != 0)
     {
-        validate[j] = email[i];
-        j++;
-        i++;
-    }
-    int result = strcmp(validate, email1);
-    int result1 = strcmp(validate, gmail);
-  
-    if (result == 0 || result1 == 0){
-        strcpy(reg.email, email);
-
-        printf("Enter password \n");
-        scanf("%s", &password1);
-        printf("Enter password once again to confirm \n");
-        scanf("%s", &password2);
-        if (strcmp(password1, password2) == 0)
-        {
-            strcpy(reg.password, password1);
-            printf("User registered successfully \n");
-        }
-        else
-        {
-            printf("Password and confirm password doesnot match!\n");
-        }
+        printf("Contents to file written successfully !\n");
     }
     else
-    {
-        printf("Email should contain @gmail.com or @email.com \n");
-    }
-     // write struct to file
-    fwrite (&reg, sizeof(struct Register), 1, user);
-   
-
-      
-    if(fwrite != 0) 
-        printf("Contents to file written successfully !\n");
-    else 
         printf("Error writing file !\n");
-  
+
     // close file
-    fclose (user);
+    fclose(user);
     return 0;
 }
