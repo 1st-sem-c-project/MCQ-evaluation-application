1ed82349c498868af85a1fd7af90b006d478ed2c
ManishGru <gurumanish2015@gmail.com>
2021-08-21 22:28:07 +0545

    code for viewing student status by admin.

--- code/change_data.h
+++ code/change_data.h
@@ -3,3 +3,28 @@ void store_in_database(struct Register user){
     fwrite(&user,sizeof(struct Register),1,auser);
     fclose(auser);
 }
+void get_student_data(struct Register *student,int *position){
+    FILE *fptr = fopen("resources/question_user_number.txt","rb");
+    int totalStudents;
+    int totalQuestion;
+    fscanf(fptr,"%d %d",&totalQuestion,&totalStudents);
+    fclose(fptr);
+    fptr = fopen("resources/username_password.txt","r");
+    for (int i = 0; i < *position; i++)
+    {
+        fread(student,sizeof(struct Register),1,fptr);
+        if((*student).admin == 1){
+            if (*position == totalStudents){
+                rewind(fptr);
+                *position = 1;
+            }
+            fread(student,sizeof(struct Register),1,fptr);
+            (*position)++;
+        }
+    }
+    fclose(fptr);
+    (*position)++;
+    if(*position>totalStudents){
+        *position = 1;
+    }
+}
